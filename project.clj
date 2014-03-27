2c064ba29cea51f0800d7cf7534ad89b7a58169e
arrdem <rmckenzie92@gmail.com>
2014-03-26 23:32:59 -0500

    Add timbre as a reqirement

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.taoensso/timbre "3.1.6"]
                  [org.clojars.gjahad/debug-repl "0.3.3"]]
   :aot [inclojure.core]
   :main inclojure.core
